d53bdec3d06e2339fb363b5e6848c9e3424efdb8
Isabella Herman <isavbherman@gmail.com>
2025-07-16 20:30:00 -0300

    route.ts fix

--- gitmon/app/api/auth/[...nextauth]/route.ts
+++ gitmon/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
 import { NextAuthOptions } from "next-auth"
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
